6f5c44b9b4d8fe74648e657eb821965eaee92f5f
Pedro Gurgel Diniz Neto <pgdneto@gmail.com>
2020-06-04 12:52:26 -0300

    Navegacao por coringas arrumada.

--- resources/views/layouts/covidnav.blade.php
+++ resources/views/layouts/covidnav.blade.php
@@ -4,25 +4,25 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="inicio">Início <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="{{ URL::to('inicio') }}">Início <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Formulários
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="agendamento">Agendamento</a>
-            <a class="dropdown-item" href="PEP">PEP - Prontuário Eletrônico</a>
-            <a class="dropdown-item" href="acompEnfermagem">Acompanhamento da Enfermagem</a>
-            <a class="dropdown-item" href="acompMedico">Acompanhamento Médico</a>
-            <a class="dropdown-item" href="acompPsico">Acompanhamento Psicológico</a>    
+            <a class="dropdown-item" href="{{ URL::to('agendamento') }}">Agendamento</a>
+            <a class="dropdown-item" href="{{ URL::to('PEP') }}">PEP - Prontuário Eletrônico</a>
+            <a class="dropdown-item" href="{{ URL::to('acompEnfermagem') }}">Acompanhamento da Enfermagem</a>
+            <a class="dropdown-item" href="{{ URL::to('acompMedico') }}">Acompanhamento Médico</a>
+            <a class="dropdown-item" href="{{ URL::to('acompPsico') }}">Acompanhamento Psicológico</a>
           </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="agendamento">Agenda</a>
+            <a class="nav-link" href="{{ URL::to('agendamento') }}">Agenda</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="consultaDados">Consulta de Dados</a>
+            <a class="nav-link" href="{{ URL::to('consultaDados') }}">Consulta de Dados</a>
         </li>
       </ul>
     </div>
